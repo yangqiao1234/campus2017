853c494008f24e10207a759b4687831d9b882fd1
joe <1018954649@qq.com>
2016-11-11 20:25:58 +0800

    create project

---
+++ EffectiveLines/src/com/qunar/homework/EffectiveLines.java
@@ -0,0 +1,12 @@
+package com.qunar.homework;
+
+/**
+ * Created by joe on 2016/11/11.
+ * 统计java代码的有效行数，
+ * 测试代码，自身
+ */
+public class EffectiveLines {
+    public static void main(String[] args) {
+
+    }
+}
